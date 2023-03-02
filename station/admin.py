a97fd8efb64ff18effd3548074e1f0d3af97530f
Tech <soran.hamad@epu.edu.iq>
2023-03-02 16:56:23 +0300

    New

--- station/admin.py
+++ station/admin.py
@@ -1,22 +1,84 @@
 from .models import *
 from django.contrib import admin
 
+from django.contrib import admin
+from django.contrib.admin import helpers
+from django.contrib.admin.options import csrf_protect_m
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
-class SalesItem(admin.TabularInline):
+
+# class SalesItem(admin.TabularInline):
+#     model = Sales
+#     extra = 1
+#
+#
+# @admin.register(S_Invoice)
+# class ProfileAdmin(admin.ModelAdmin):
+#     inlines = [SalesItem]
+#
+#     class Meta:
+#         model = S_Invoice
+#
+#     list_display = ('invoice_number', 'total_sales_amount', 'get_station_numbers')
+#
+#     def get_station_numbers(self, obj):
+#         return ", ".join(obj.get_station_numbers())
+#
+#     get_station_numbers.short_description = "Station Numbers"
+class SalesAdminInline(admin.TabularInline):
     model = Sales
     extra = 1
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "gas_station":
+            if request.user.is_superuser:
+                pass
+            else:
+                kwargs["queryset"] = GasStation.objects.filter(user=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 @admin.register(S_Invoice)
-class ProfileAdmin(admin.ModelAdmin):
-    inlines = [SalesItem]
+class S_InvoiceAdmin(admin.ModelAdmin):
+    inlines = [SalesAdminInline]
 
-    class Meta:
-        model = S_Invoice
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        if request.method == 'POST':
+            try:
+                return super().changeform_view(request, object_id=object_id, form_url=form_url,
+                                               extra_context=extra_context)
+            except ValueError as error:
+                self.message_user(request, _(str(error)), level='ERROR')
+                url = reverse('admin:%s_%s_change' % (self.opts.app_label, self.opts.model_name), args=[object_id])
+                return HttpResponseRedirect(url)
+        else:
+            return super().changeform_view(request, object_id=object_id, form_url=form_url, extra_context=extra_context)
 
-    # list_display = ('invoice_number', 'customer_name')
+    def show_sales_total(self, obj):
+        total_sales = sum(sale.total_amount for sale in obj.sales.all())
+        return format_html('<b>{}</b>', total_sales)
+
+    def show_station_number(self, obj):
+        station_number = obj.sales.first().gas_station.station if obj.sales.first() else '-'
+        return format_html('<b>{}</b>', station_number)
+
+    show_sales_total.short_description = _('Total Sales')
+    show_station_number.short_description = _('Station Number')
+
+    # list_display = ('invoice_number', 'show_sales_total', 'show_station_number')
+    # search_fields = ('invoice_number', 'sales__gas_station__station_number')
+
+    list_display = ('invoice_number', 'total_sales_amount', 'get_station_numbers')
 
 
+#
+#     def get_station_numbers(self, obj):
+#         return ", ".join(obj.get_station_numbers())
+#
+#     get_station_numbers.short_description = "Station Numbers"
 class StockItem(admin.TabularInline):
     model = Stock
     extra = 1
@@ -49,6 +111,7 @@ class StockAdmin(admin.ModelAdmin):
 
 
 class SalesAdmin(admin.ModelAdmin):
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "gas_station":
             # Show only the gas station associated with the current user
--- station/models.py
+++ station/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 
 class station(models.Model):
@@ -51,6 +52,21 @@ class S_Invoice(models.Model):
     def __str__(self):
         return f" Invoice Number : {self.invoice_number}"
 
+    def total_sales_amount(self):
+        total_sales_amount = self.sales.aggregate(total=Sum('total_amount'))['total']
+        return total_sales_amount or 0
+
+    def get_station_numbers(self):
+        station_numbers = [sale.gas_station.station for sale in self.sales.all()]
+        print(f"station_numbers: {station_numbers}")
+        station_numbers = list(set(station_numbers))
+        print(f"station_numbers (unique): {station_numbers}")
+        station_numbers = [str(station) for station in station_numbers]
+        print(f"station_numbers (as strings): {station_numbers}")
+        return ", ".join(station_numbers)
+
+    get_station_numbers.short_description = "Station Numbers"
+
 
 class Sales(models.Model):
     sales_invoice = models.ForeignKey(S_Invoice, on_delete=models.CASCADE, related_name='sales')
@@ -68,16 +84,17 @@ class Sales(models.Model):
         try:
             stock = Stock.objects.get(gas_station=self.gas_station, item=self.item)
         except Stock.DoesNotExist:
-            raise ValueError(f"{self.item.name} is not in stock for {self.gas_station.station.name}")
+            raise ValueError(f"{self.item.name} is not in stock for station  {self.gas_station.station.name}")
 
         if self.quantity > stock.quantity:
             raise ValueError(
-                f"Only {stock.quantity} units of {self.item.name} are in stock for {self.gas_station.station.name}")
+                f"Sorry, we don't have enough {self.item.name} in stock  for station   {self.gas_station.station.name} right now. "
+                f"Please reduce your order quantity to {stock.quantity} or less."
+            )
 
         self.total_amount = self.price * self.quantity
         super().save(*args, **kwargs)
 
         # Update the stock quantity
         stock.quantity -= self.quantity
-
         stock.save()
