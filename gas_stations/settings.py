6281e7407f868552eabe529554b6aff4b70255ee
Tech <soran.hamad@epu.edu.iq>
2023-03-19 14:52:18 +0300

    New-19-3-2023

--- gas_stations/settings.py
+++ gas_stations/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -74,10 +74,22 @@ WSGI_APPLICATION = "gas_stations.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddu7hf3179chle',
+        'USER': 'ielhlpzqicyqog',
+        'PASSWORD': 'acbeb73fbacb415e2725ab7f6b28bb90c21d7b3f16d35180185bd0ffa0b385ba',
+        'HOST': 'ec2-54-208-11-146.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -104,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Baghdad'
 
 USE_I18N = True
 
