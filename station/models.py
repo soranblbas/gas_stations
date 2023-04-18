3a634b309bd1fa05979dc8efead8e429871aef6c
Tech <soran.hamad@epu.edu.iq>
2023-04-18 20:58:03 +0300

    New-19-3-2023

--- station/models.py
+++ station/models.py
@@ -223,7 +223,7 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     order_delivered = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=13,editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=13, editable=False)
 
     class Meta:
         verbose_name_plural = 'داواكردن'
@@ -258,9 +258,9 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name="Set",default=0)
-    note = models.CharField(blank=True, verbose_name="write your name?", max_length=50,null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=13,editable=False)
+    quantity = models.PositiveIntegerField(verbose_name="Set", default=0)
+    note = models.CharField(blank=True, verbose_name="write your name?", max_length=50, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=13, editable=False)
 
     def __str__(self):
         return f"{self.item.name} - {self.quantity}"
