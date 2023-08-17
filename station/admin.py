ae83ac7ec0652eaae0aa1444f52ea359d4a418f6
soranblbas <76163231+soranblbas@users.noreply.github.com>
2023-08-17 16:39:28 +0300

    last

--- station/admin.py
+++ station/admin.py
@@ -238,7 +238,6 @@ class StockAdmin(admin.ModelAdmin):
         return obj.stock_invoice.created_at
 
     stock_invoice_created_at.short_description = 'Stock Invoice Created At'
-
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
